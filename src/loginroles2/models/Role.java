6068fd31b8a83c52a1c5c53472b9769fa8a5a008
ItsRicmor <ricardojms1999@gmail.com>
2019-09-12 09:15:29 -0600

    First Commit

---
+++ src/loginroles2/models/Role.java
@@ -0,0 +1,5 @@
+package loginroles2.models;
+
+public enum Role {
+    ROLE_ADMIN, ROLE_CLIENT
+}
---
+++ src/loginroles2/models/User.java
@@ -0,0 +1,53 @@
+package loginroles2.models;
+
+public class User {
+    
+    private String email;
+    private String password;
+    private Person person;
+    private Role role;
+
+    public User() {
+    }
+
+    public User(String email, String password, Person person, Role role) {
+        this.email = email;
+        this.password = password;
+        this.person = person;
+        this.role = role;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public void setPerson(Person person) {
+        this.person = person;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+    
+    
+}
